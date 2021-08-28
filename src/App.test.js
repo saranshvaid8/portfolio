d608c914eee2079c5cfe2874379eb13ef030d8d3
saransh vaid <saranshvaid8@gmail.com>
2021-08-28 17:13:12 -0400

    updated test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Saransh is learning react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/saransh/i);
   expect(linkElement).toBeInTheDocument();
 });
